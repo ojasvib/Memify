e7fb53a559a1afc451d4f319101754e1e62497da
Sanskar Agrawal <sanskar10100@gmail.com>
2024-09-17 21:49:27 +0530

    fix layout and offset

--- app/src/main/java/com/ojasvi/memify/ui/screens/PhotoReasoningScreen.kt
+++ app/src/main/java/com/ojasvi/memify/ui/screens/PhotoReasoningScreen.kt
@@ -4,9 +4,11 @@ import android.net.Uri
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.PickVisualMediaRequest
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.background
 import androidx.compose.foundation.gestures.detectDragGestures
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ColumnScope
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -14,6 +16,7 @@ import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.Add
@@ -36,7 +39,10 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.pointer.consumeAllChanges
 import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
@@ -45,6 +51,7 @@ import coil.compose.AsyncImage
 import com.ojasvi.memify.PhotoReasoningUiState
 import com.ojasvi.memify.PhotoReasoningViewModel
 import com.ojasvi.memify.R
+import org.intellij.lang.annotations.PrintFormat
 import kotlin.math.roundToInt
 
 @Composable
@@ -66,69 +73,53 @@ fun PhotoReasoningContents(
     uiState: PhotoReasoningUiState = PhotoReasoningUiState.Loading,
     onReasonClicked: (String, Uri) -> Unit = { _, _ -> }
 ) {
-    var userQuestion by rememberSaveable { mutableStateOf("") }
-    var imageUri by rememberSaveable { mutableStateOf<Uri?>(null) }
-
-    val pickMedia = rememberLauncherForActivityResult(
-        ActivityResultContracts.PickVisualMedia()
-    ) { selectedImageUri ->
-        selectedImageUri?.let {
-            imageUri = selectedImageUri
-        }
-    }
+    var imageUri by rememberSaveable { mutableStateOf<Uri>(Uri.EMPTY) }
+    var prompt by rememberSaveable { mutableStateOf("") }
 
     Column(
-        modifier = Modifier
-            .padding(all = 16.dp)
-            .verticalScroll(rememberScrollState())
+        modifier = Modifier.fillMaxSize()
     ) {
-        var offsetX by remember { mutableFloatStateOf(0f) }
-        var offsetY by remember { mutableFloatStateOf(0f) }
-
-        var startX by remember { mutableFloatStateOf(0f) }
-        var startY by remember { mutableFloatStateOf(0f) }
-
-
-        Box(modifier = Modifier.fillMaxSize(0.8f).align(Alignment.CenterHorizontally)) {
-            AsyncImage(model = imageUri, contentDescription = "", modifier = Modifier.fillMaxSize())
+        Box(
+            modifier = Modifier
+                .weight(1f)
+                .padding(16.dp)
+        ) {
+            AsyncImage(
+                model = imageUri,
+                contentDescription = "",
+                modifier = Modifier.fillMaxSize(),
+                contentScale = ContentScale.Crop
+            )
 
             when (uiState) {
-                PhotoReasoningUiState.Initial -> {
-                    // Nothing is shown
-                }
+                PhotoReasoningUiState.Initial -> {}
 
                 PhotoReasoningUiState.Loading -> {
-                    Box(
-                        contentAlignment = Alignment.Center,
-                        modifier = Modifier
-                            .padding(all = 8.dp)
-                    ) {
-                        CircularProgressIndicator()
-                    }
+                    CircularProgressIndicator(
+                        modifier = Modifier.align(Alignment.Center)
+                    )
                 }
 
                 is PhotoReasoningUiState.Success -> {
+                    var offsetX by remember { mutableFloatStateOf(0f) }
+                    var offsetY by remember { mutableFloatStateOf(0f) }
+
                     Card(
+                        shape = RoundedCornerShape(16.dp),
+                        colors = CardDefaults.cardColors(
+                            containerColor = MaterialTheme.colorScheme.onSecondaryContainer
+                        ),
                         modifier = Modifier
-                            .padding(vertical = 16.dp)
-                            .fillMaxWidth(0.7f)
+                            .align(Alignment.BottomCenter)
                             .offset { IntOffset(offsetX.roundToInt(), offsetY.roundToInt()) } // Move the button
                             .pointerInput(Unit) {
                                 detectDragGestures(
-                                    onDragStart = { offset ->
-                                        startX = offsetX
-                                        startY = offsetY
-                                    },
-                                    onDrag = { change, dragAmount ->
-                                        offsetX = startX + dragAmount.x
-                                        offsetY = startY + dragAmount.y
+                                    onDrag = { _, dragAmount ->
+                                        offsetX += dragAmount.x
+                                        offsetY += dragAmount.y
                                     }
                                 )
-                            },
-                        shape = MaterialTheme.shapes.large,
-                        colors = CardDefaults.cardColors(
-                            containerColor = MaterialTheme.colorScheme.onSecondaryContainer
-                        )
+                            }
                     ) {
                         Row(
                             modifier = Modifier
@@ -166,54 +157,68 @@ fun PhotoReasoningContents(
             }
         }
 
-        Card(
-            modifier = Modifier.fillMaxWidth()
+        InputField(
+            prompt = prompt,
+            onPromptChanged = { prompt = it },
+            onSendClicked = { onReasonClicked(prompt, imageUri) },
+            onImageSelected = { imageUri = it }
+        )
+    }
+}
+
+@Composable
+private fun ColumnScope.InputField(
+    prompt: String,
+    onPromptChanged: (String) -> Unit,
+    onSendClicked: () -> Unit,
+    onImageSelected: (Uri) -> Unit,
+) {
+    val pickMedia = rememberLauncherForActivityResult(
+        ActivityResultContracts.PickVisualMedia()
+    ) { selectedImageUri ->
+        selectedImageUri?.let { onImageSelected(it) }
+    }
+
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+    ) {
+        Row(
+            modifier = Modifier
+                .padding(vertical = 14.dp)
+                .navigationBarsPadding()
         ) {
-            Row(
-                modifier = Modifier.padding(vertical = 14.dp)
-            ) {
-                IconButton(
-                    onClick = {
-                        pickMedia.launch(
-                            PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly)
-                        )
-                    },
-                    modifier = Modifier
-                        .padding(all = 4.dp)
-                        .align(Alignment.CenterVertically)
-                ) {
-                    Icon(
-                        Icons.Rounded.Add,
-                        contentDescription = stringResource(R.string.add_image),
+            IconButton(
+                onClick = {
+                    pickMedia.launch(
+                        PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly)
                     )
-                }
-                OutlinedTextField(
-                    value = userQuestion,
-                    label = { Text(stringResource(R.string.reason_label)) },
-                    placeholder = { Text(stringResource(R.string.reason_hint)) },
-                    onValueChange = { userQuestion = it },
-                    modifier = Modifier
-                        .fillMaxWidth(0.8f)
+                },
+                modifier = Modifier
+                    .padding(all = 4.dp)
+                    .align(Alignment.CenterVertically)
+            ) {
+                Icon(
+                    Icons.Rounded.Add,
+                    contentDescription = stringResource(R.string.add_image),
                 )
-                TextButton(
-                    onClick = {
-                        imageUri?.let {
-                            onReasonClicked(
-                                "$userQuestion Generate a meme caption that fits this photo. The response should contain only the caption without any quotes.",
-                                it
-                            )
-                        }
-                    },
-                    modifier = Modifier
-                        .padding(all = 4.dp)
-                        .align(Alignment.CenterVertically)
-                ) {
-                    Text(stringResource(R.string.action_go))
-                }
             }
-
+            OutlinedTextField(
+                value = prompt,
+                label = { Text(stringResource(R.string.reason_label)) },
+                placeholder = { Text(stringResource(R.string.reason_hint)) },
+                onValueChange = { onPromptChanged(it) },
+                modifier = Modifier
+                    .fillMaxWidth(0.8f)
+            )
+            TextButton(
+                onClick = { onSendClicked() },
+                modifier = Modifier
+                    .padding(all = 4.dp)
+                    .align(Alignment.CenterVertically)
+            ) {
+                Text(stringResource(R.string.action_go))
+            }
         }
-
-
     }
 }
