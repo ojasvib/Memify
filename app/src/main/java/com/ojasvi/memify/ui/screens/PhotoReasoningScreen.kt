c059b3d246510a0eb7431558422f068069d97dd4
Ojasvi <ojasvibhargava7@gmail.com>
2024-09-17 21:27:56 +0530

    Add caption draggable text

--- app/src/main/java/com/ojasvi/memify/ui/screens/PhotoReasoningScreen.kt
+++ app/src/main/java/com/ojasvi/memify/ui/screens/PhotoReasoningScreen.kt
@@ -4,43 +4,48 @@ import android.net.Uri
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.PickVisualMediaRequest
 import androidx.activity.result.contract.ActivityResultContracts
-import androidx.compose.foundation.clickable
+import androidx.compose.foundation.gestures.detectDragGestures
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.navigationBarsPadding
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.requiredSize
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.outlined.Person
 import androidx.compose.material.icons.rounded.Add
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableFloatStateOf
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.drawBehind
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import coil.compose.AsyncImage
 import com.ojasvi.memify.PhotoReasoningUiState
 import com.ojasvi.memify.PhotoReasoningViewModel
 import com.ojasvi.memify.R
+import kotlin.math.roundToInt
 
 @Composable
 fun PhotoReasoningScreen(
@@ -77,32 +82,111 @@ fun PhotoReasoningContents(
             .padding(all = 16.dp)
             .verticalScroll(rememberScrollState())
     ) {
-        AsyncImage(
-            model = imageUri,
-            contentDescription = null,
-            modifier = Modifier
-                .padding(4.dp)
-                .fillMaxWidth(0.5f)
-                .align(Alignment.CenterHorizontally)
-        )
+        var offsetX by remember { mutableFloatStateOf(0f) }
+        var offsetY by remember { mutableFloatStateOf(0f) }
+
+        var startX by remember { mutableFloatStateOf(0f) }
+        var startY by remember { mutableFloatStateOf(0f) }
+
+
+        Box(modifier = Modifier.fillMaxSize(0.8f).align(Alignment.CenterHorizontally)) {
+            AsyncImage(model = imageUri, contentDescription = "", modifier = Modifier.fillMaxSize())
+
+            when (uiState) {
+                PhotoReasoningUiState.Initial -> {
+                    // Nothing is shown
+                }
+
+                PhotoReasoningUiState.Loading -> {
+                    Box(
+                        contentAlignment = Alignment.Center,
+                        modifier = Modifier
+                            .padding(all = 8.dp)
+                    ) {
+                        CircularProgressIndicator()
+                    }
+                }
+
+                is PhotoReasoningUiState.Success -> {
+                    Card(
+                        modifier = Modifier
+                            .padding(vertical = 16.dp)
+                            .fillMaxWidth(0.7f)
+                            .offset { IntOffset(offsetX.roundToInt(), offsetY.roundToInt()) } // Move the button
+                            .pointerInput(Unit) {
+                                detectDragGestures(
+                                    onDragStart = { offset ->
+                                        startX = offsetX
+                                        startY = offsetY
+                                    },
+                                    onDrag = { change, dragAmount ->
+                                        offsetX = startX + dragAmount.x
+                                        offsetY = startY + dragAmount.y
+                                    }
+                                )
+                            },
+                        shape = MaterialTheme.shapes.large,
+                        colors = CardDefaults.cardColors(
+                            containerColor = MaterialTheme.colorScheme.onSecondaryContainer
+                        )
+                    ) {
+                        Row(
+                            modifier = Modifier
+                                .padding(all = 16.dp)
+                                .fillMaxWidth()
+                        ) {
+                            Text(
+                                text = uiState.outputText, // TODO(thatfiredev): Figure out Markdown support
+                                color = MaterialTheme.colorScheme.onSecondary,
+                                modifier = Modifier
+                                    .padding(start = 16.dp)
+                                    .fillMaxWidth()
+                            )
+                        }
+                    }
+                }
+
+                is PhotoReasoningUiState.Error -> {
+                    Card(
+                        modifier = Modifier
+                            .padding(vertical = 16.dp)
+                            .fillMaxWidth(),
+                        shape = MaterialTheme.shapes.large,
+                        colors = CardDefaults.cardColors(
+                            containerColor = MaterialTheme.colorScheme.errorContainer
+                        )
+                    ) {
+                        Text(
+                            text = uiState.errorMessage,
+                            color = MaterialTheme.colorScheme.error,
+                            modifier = Modifier.padding(all = 16.dp)
+                        )
+                    }
+                }
+            }
+        }
+
         Card(
             modifier = Modifier.fillMaxWidth()
         ) {
             Row(
-                modifier = Modifier.padding(top = 16.dp)
+                modifier = Modifier.padding(vertical = 14.dp)
             ) {
-                Icon(
-                    Icons.Rounded.Add,
-                    contentDescription = stringResource(R.string.add_image),
+                IconButton(
+                    onClick = {
+                        pickMedia.launch(
+                            PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly)
+                        )
+                    },
                     modifier = Modifier
                         .padding(all = 4.dp)
                         .align(Alignment.CenterVertically)
-                        .clickable {
-                            pickMedia.launch(
-                                PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly)
-                            )
-                        }
-                )
+                ) {
+                    Icon(
+                        Icons.Rounded.Add,
+                        contentDescription = stringResource(R.string.add_image),
+                    )
+                }
                 OutlinedTextField(
                     value = userQuestion,
                     label = { Text(stringResource(R.string.reason_label)) },
@@ -111,89 +195,25 @@ fun PhotoReasoningContents(
                     modifier = Modifier
                         .fillMaxWidth(0.8f)
                 )
-                Text(text = stringResource(R.string.action_go),
+                TextButton(
+                    onClick = {
+                        imageUri?.let {
+                            onReasonClicked(
+                                "$userQuestion Generate a meme caption that fits this photo. The response should contain only the caption without any quotes.",
+                                it
+                            )
+                        }
+                    },
                     modifier = Modifier
                         .padding(all = 4.dp)
                         .align(Alignment.CenterVertically)
-                        .clickable {
-                            if (userQuestion.isNotBlank()) {
-                                imageUri?.let { onReasonClicked(userQuestion, it) }
-                            }
-                        }
-                )
+                ) {
+                    Text(stringResource(R.string.action_go))
+                }
             }
 
         }
 
-        when (uiState) {
-            PhotoReasoningUiState.Initial -> {
-                // Nothing is shown
-            }
 
-            PhotoReasoningUiState.Loading -> {
-                Box(
-                    contentAlignment = Alignment.Center,
-                    modifier = Modifier
-                        .padding(all = 8.dp)
-                        .align(Alignment.CenterHorizontally)
-                ) {
-                    CircularProgressIndicator()
-                }
-            }
-
-            is PhotoReasoningUiState.Success -> {
-                Card(
-                    modifier = Modifier
-                        .padding(vertical = 16.dp)
-                        .fillMaxWidth(),
-                    shape = MaterialTheme.shapes.large,
-                    colors = CardDefaults.cardColors(
-                        containerColor = MaterialTheme.colorScheme.onSecondaryContainer
-                    )
-                ) {
-                    Row(
-                        modifier = Modifier
-                            .padding(all = 16.dp)
-                            .fillMaxWidth()
-                    ) {
-                        Icon(
-                            Icons.Outlined.Person,
-                            contentDescription = "Person Icon",
-                            tint = MaterialTheme.colorScheme.onSecondary,
-                            modifier = Modifier
-                                .requiredSize(36.dp)
-                                .drawBehind {
-                                    drawCircle(color = Color.White)
-                                }
-                        )
-                        Text(
-                            text = uiState.outputText, // TODO(thatfiredev): Figure out Markdown support
-                            color = MaterialTheme.colorScheme.onSecondary,
-                            modifier = Modifier
-                                .padding(start = 16.dp)
-                                .fillMaxWidth()
-                        )
-                    }
-                }
-            }
-
-            is PhotoReasoningUiState.Error -> {
-                Card(
-                    modifier = Modifier
-                        .padding(vertical = 16.dp)
-                        .fillMaxWidth(),
-                    shape = MaterialTheme.shapes.large,
-                    colors = CardDefaults.cardColors(
-                        containerColor = MaterialTheme.colorScheme.errorContainer
-                    )
-                ) {
-                    Text(
-                        text = uiState.errorMessage,
-                        color = MaterialTheme.colorScheme.error,
-                        modifier = Modifier.padding(all = 16.dp)
-                    )
-                }
-            }
-        }
     }
 }
